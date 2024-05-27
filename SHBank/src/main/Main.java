56eb9149b86370d4d3de603fa3c5517489dca882
SylvainHu <sylvainhupro@gmail.com>
2024-05-27 13:37:57 +0200

    Added method to create hashtables

--- SHBank/src/main/Main.java
+++ SHBank/src/main/Main.java
@@ -1,9 +1,11 @@
 // 1.1.2 Creation of main class for tests
 // 1.2.3 Creation of the tablea account
+// 1.3.1 Adaptation of the table of accounts
 
 package main;
 
 import java.util.ArrayList;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,13 +19,13 @@ public class Main {
 	public static void main(String[] args) {
 
 		List<Client> clients = generateClients(3);
-
 		displayClients(clients);
 
 		List<Account> accounts = generateAccounts(clients);
+//		displayAccounts(accounts);
 
-		displayAccounts(accounts);
-
+		Hashtable<Integer, Account> accountTable = createAccountTable(accounts);
+		displayAccountTable(accountTable);
 	}
 
 	public static List<Client> generateClients(int numberOfClients) {
@@ -43,6 +45,7 @@ public class Main {
 		System.out.println(clientDetails);
 	}
 
+	// 1.2.3 Creation of the tablea account
 	public static List<Account> generateAccounts(List<Client> clients) {
 
 		List<Account> accounts = new ArrayList<>();
@@ -61,4 +64,27 @@ public class Main {
 		System.out.println(accountDetails);
 	}
 
+	// 1.3.1 Adaptation of the table of accounts
+	public static Hashtable<Integer, Account> createAccountTable(List<Account> accounts) {
+		Hashtable<Integer, Account> accountTable = new Hashtable<>();
+
+		for (Account account : accounts) {
+			accountTable.put(account.getAccountNumber(), account);
+		}
+
+		return accountTable;
+	}
+
+	public static void displayAccountTable(Hashtable<Integer, Account> accountTable) {
+//		String tableDetails = accountTable.values().stream()
+//				.sorted((a1, a2) -> Double.compare(a1.getBalance(), a2.getBalance())).map(Account::toString)
+//				.collect(Collectors.joining("\n"));
+
+		String tableDetails = accountTable.values().stream().sorted((a1, a2) -> {
+			int balanceComparison = Double.compare(a1.getBalance(), a2.getBalance());
+			return balanceComparison != 0 ? balanceComparison
+					: Integer.compare(a1.getAccountNumber(), a2.getAccountNumber());
+		}).map(Account::toString).collect(Collectors.joining("\n"));
+		System.out.println(tableDetails);
+	}
 }
